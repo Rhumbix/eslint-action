3a664ce4158f490c8f28d46528f335a09e569734
Liv Carman <ocarman@rhumbix.com>
2020-04-16 10:15:25 -0700

    Only check files from PR, install private packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,27 +3,17 @@
 # Exit if any subcommand fails
 set -e 
 
-# Setup node modules if needed
-if [ -e node_modules/.bin/eslint ]; then
-    setup=""
-else
-    echo "## Your environment is not ready yet. Installing modules..."
-    if [ -f yarn.lock ]; then
-        setup="yarn --non-interactive --silent --ignore-scripts --production=false &&"
-    else
-        if [ -f package-lock.json ]; then
-            setup="NODE_ENV=development npm ci --ignore-scripts &&"
-        else
-            setup="NODE_ENV=development npm install --no-package-lock --ignore-scripts &&"
-        fi
-    fi
-fi
+# Install dependencies
+yarn --non-interactive --silent --ignore-scripts --production=false
 
-if [ -z "$1" ]; then
-    glob="."
-else 
-    glob="$@"
-fi
+# Run ESLint on files that were modified (but not deleted) by this PR
+ESLINT="./node_modules/.bin/eslint"
+CHANGED_FILES=$(git diff --name-only --diff-filter=ACM ${{ github.head_ref }}..${{github.base_ref}} | grep -E "\\.[jt]sx?$")
 
 echo "## Running ESLint"
-sh -c "$setup ./node_modules/.bin/eslint $glob"
+if [[ "$CHANGED_FILES" != "" ]]; then
+
+    for FILE in $CHANGED_FILES; do
+        "$ESLINT" "$FILE"
+    done
+fi
